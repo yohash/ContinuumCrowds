5f7a4de8bf180aefce6423e1c0d287f472249c30
yohash <jdheebl@gmail.com>
2022-12-27 21:46:24 +1300

    added namespace to all continuum crowds core scripts

--- Classes/CcTileExtensions.cs
+++ Classes/CcTileExtensions.cs
@@ -1,33 +1,37 @@
-public static class CcTileExtensions
+
+namespace Yohash.ContinuumCrowds
 {
-  public static bool ContainsLocalPoint(this CcTile tile, int x, int y)
+  public static class CcTileExtensions
   {
-    return 0 <= x && x <= tile.SizeX - 1
-        && 0 <= y && y <= tile.SizeY - 1;
-  }
-
-  public static bool ContainsGlobalPoint(this CcTile tile, Location l)
-  {
-    return tile.ContainsGlobalPoint(l.x, l.y);
-  }
-
-  public static bool ContainsGlobalPoint(this CcTile tile, int x, int y)
-  {
-    return tile.Corner.x <= x && x <= tile.Corner.x + tile.SizeX - 1
-        && tile.Corner.y <= y && y <= tile.Corner.y + tile.SizeY - 1;
-  }
-
-  public static bool IsLocalPointValid(this CcTile tile, int x, int y)
-  {
-    // check to make sure the point is not outside the tile
-    if (!tile.ContainsLocalPoint(x, y)) {
-      return false;
-    }
-    // check to see if the point is in a place of absolute discomfort
-    if (tile.g[x, y] >= 1) {
-      return false;
+    public static bool ContainsLocalPoint(this CcTile tile, int x, int y)
+    {
+      return 0 <= x && x <= tile.SizeX - 1
+          && 0 <= y && y <= tile.SizeY - 1;
     }
 
-    return true;
+    public static bool ContainsGlobalPoint(this CcTile tile, Location l)
+    {
+      return tile.ContainsGlobalPoint(l.x, l.y);
+    }
+
+    public static bool ContainsGlobalPoint(this CcTile tile, int x, int y)
+    {
+      return tile.Corner.x <= x && x <= tile.Corner.x + tile.SizeX - 1
+          && tile.Corner.y <= y && y <= tile.Corner.y + tile.SizeY - 1;
+    }
+
+    public static bool IsLocalPointValid(this CcTile tile, int x, int y)
+    {
+      // check to make sure the point is not outside the tile
+      if (!tile.ContainsLocalPoint(x, y)) {
+        return false;
+      }
+      // check to see if the point is in a place of absolute discomfort
+      if (tile.g[x, y] >= 1) {
+        return false;
+      }
+
+      return true;
+    }
   }
 }
--- Classes/CcUnitPath.cs
+++ Classes/CcUnitPath.cs
@@ -2,100 +2,103 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-public class CcUnitPath
+namespace Yohash.ContinuumCrowds
 {
-  // IF the unit is on a tile that doesn't exist in this solution,
-  //      we need to determine the best way back to the nearest tile
-
-  // maintain a linked list of destinations as well, so we can try
-  // daisy-chaining our references
-  private LinkedList<CcDestination> destinations;
-
-  /// <summary>
-  /// Tracking the current destination
-  /// </summary>
-  private LinkedListNode<CcDestination> _current;
-  public CcDestination CurrentDestination {
-    get { return _current.Value; }
-  }
-
-  /// <summary>
-  /// This path is the ordered set of CcDestinations that must
-  /// be traversed in order to arrive at a goal
-  /// </summary>
-  /// <param name="portals"></param>
-  /// <param name="navSystem"></param>
-  public CcUnitPath(
-    IEnumerable<Portal> portals,
-    Dictionary<Location, CcTile> tiles,
-    Func<Location, Location> hashTileLocation
-  )
+  public class CcUnitPath
   {
-    destinations = new LinkedList<CcDestination>();
+    // IF the unit is on a tile that doesn't exist in this solution,
+    //      we need to determine the best way back to the nearest tile
 
-    foreach (var portal in portals) {
-      // see if this is the starting or end portal
-      var singular = portal.borderA.SequenceEqual(portal.borderB);
-      if (singular && destinations.Count == 0) {
-        var hashed = hashTileLocation(portal.borderA[0]);
-        var tiled = tiles[hashed];
-        var dest = new CcDestination(
-          tiled,
-          portal.borderA.ToList()
-        );
+    // maintain a linked list of destinations as well, so we can try
+    // daisy-chaining our references
+    private LinkedList<CcDestination> destinations;
 
-        // this is the start
-        if (destinations.Count == 0) {
-          destinations.AddFirst(dest);
-        } else {
-          // this is the ending portal
-          destinations.AddLast(dest);
-        }
-        continue;
-      }
-      // see if this is the ending portal
-      else if (singular) {
-        // this is the start
-        var hashed = hashTileLocation(portal.borderA[0]);
-        var tiled = tiles[hashed];
-        var desty = new CcDestination(
-          tiled,
-          portal.borderA.ToList()
-        );
-        destinations.AddLast(desty);
-        continue;
-      }
-
-      // based on the location from which we're coming, choose which
-      // portal we want
-
-      // (1) look at the most recent destination and grab the tile
-      var lastTile = tiles[destinations.Last().TileHash];
-      // (2) compare both borders in this portal to this tile
-      var nextPortal =
-        lastTile.ContainsGlobalPoint(portal.borderA[0])
-          // (a) our last tile bontains border A, we want to path to border B
-          ? portal.borderB
-          // (b) our last tiles doesn't contain border B, so we path to A
-          : portal.borderA;
-
-      // grab the next tile based on the chosen portal,
-      var nextTile = tiles[nextPortal[0]];
-      var destination = new CcDestination(
-        // TODO - VERIFY that I can path off an edge like this
-        lastTile,
-        new List<Location>(nextPortal)
-      );
-      destinations.AddLast(destination);
+    /// <summary>
+    /// Tracking the current destination
+    /// </summary>
+    private LinkedListNode<CcDestination> _current;
+    public CcDestination CurrentDestination {
+      get { return _current.Value; }
     }
 
-    // initialize this class
-    _current = destinations.First;
-  }
+    /// <summary>
+    /// This path is the ordered set of CcDestinations that must
+    /// be traversed in order to arrive at a goal
+    /// </summary>
+    /// <param name="portals"></param>
+    /// <param name="navSystem"></param>
+    public CcUnitPath(
+      IEnumerable<Portal> portals,
+      Dictionary<Location, CcTile> tiles,
+      Func<Location, Location> hashTileLocation
+    )
+    {
+      destinations = new LinkedList<CcDestination>();
+
+      foreach (var portal in portals) {
+        // see if this is the starting or end portal
+        var singular = portal.borderA.SequenceEqual(portal.borderB);
+        if (singular && destinations.Count == 0) {
+          var hashed = hashTileLocation(portal.borderA[0]);
+          var tiled = tiles[hashed];
+          var dest = new CcDestination(
+            tiled,
+            portal.borderA.ToList()
+          );
+
+          // this is the start
+          if (destinations.Count == 0) {
+            destinations.AddFirst(dest);
+          } else {
+            // this is the ending portal
+            destinations.AddLast(dest);
+          }
+          continue;
+        }
+        // see if this is the ending portal
+        else if (singular) {
+          // this is the start
+          var hashed = hashTileLocation(portal.borderA[0]);
+          var tiled = tiles[hashed];
+          var desty = new CcDestination(
+            tiled,
+            portal.borderA.ToList()
+          );
+          destinations.AddLast(desty);
+          continue;
+        }
+
+        // based on the location from which we're coming, choose which
+        // portal we want
+
+        // (1) look at the most recent destination and grab the tile
+        var lastTile = tiles[destinations.Last().TileHash];
+        // (2) compare both borders in this portal to this tile
+        var nextPortal =
+          lastTile.ContainsGlobalPoint(portal.borderA[0])
+            // (a) our last tile bontains border A, we want to path to border B
+            ? portal.borderB
+            // (b) our last tiles doesn't contain border B, so we path to A
+            : portal.borderA;
+
+        // grab the next tile based on the chosen portal,
+        var nextTile = tiles[nextPortal[0]];
+        var destination = new CcDestination(
+          // TODO - VERIFY that I can path off an edge like this
+          lastTile,
+          new List<Location>(nextPortal)
+        );
+        destinations.AddLast(destination);
+      }
+
+      // initialize this class
+      _current = destinations.First;
+    }
 
 
-  public void Advance()
-  {
-    _current = _current.Next;
+    public void Advance()
+    {
+      _current = _current.Next;
+    }
   }
 }
