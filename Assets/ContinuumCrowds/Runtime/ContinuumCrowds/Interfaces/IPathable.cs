2e271b42c4ed921a4c967662dea2b7baed437e1e
Jason Heebl <jdheebl@gmail.com>
2024-02-24 01:02:34 +1300

    Co-locate some scripts from other repositories

---
+++ Assets/ContinuumCrowds/Runtime/ContinuumCrowds/Interfaces/IPathable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Yohash.Tools
+{
+  public interface IPathable
+  {
+    IEnumerable<IPathable> Neighbors();
+    float Heuristic(Location endGoal);
+    float Cost(IPathable neighbor);
+    Location AsLocation();
+  }
+}
